0d304ce6d433c6338cc3425f303ab8cf7d25bc81
Sameer Kulkarni <sameerkitkat4.4@gmail.com>
2020-07-25 22:59:18 +0530

    Leetcode #1342 Number of Steps to Reduce a Number to Zero

--- Number of steps to reduce to zero.py
+++ Number of steps to reduce to zero.py
@@ -17,6 +17,7 @@ Step 3) 6 is even; divide by 2 and obtain 3.
 Step 4) 3 is odd; subtract 1 and obtain 2.
 Step 5) 2 is even; divide by 2 and obtain 1.
 Step 6) 1 is odd; subtract 1 and obtain 0.
+
 Example 2:
 
 Input: num = 8
@@ -26,6 +27,7 @@ Step 1) 8 is even; divide by 2 and obtain 4.
 Step 2) 4 is even; divide by 2 and obtain 2.
 Step 3) 2 is even; divide by 2 and obtain 1.
 Step 4) 1 is odd; subtract 1 and obtain 0.
+
 Example 3:
 
 Input: num = 123
